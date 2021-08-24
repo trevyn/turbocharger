389adf9064fd72fca2a83ef50b7ba29b8481cd37
trevyn <trevyn-git@protonmail.com>
2021-08-24 12:29:50 +0400

    refine the ideal

--- example-minimal/frontend/src/index.js
+++ example-minimal/frontend/src/index.js
@@ -1,4 +1,4 @@
-import turbocharger_init, * as tc from "./turbocharger_generated";
+import turbocharger_init, { wasm, backend } from "./turbocharger_generated";
 
 function append(t) {
  document.body.appendChild(document.createTextNode(t));
@@ -7,8 +7,8 @@ function append(t) {
 async function main() {
  append("Hello from JS.");
  await turbocharger_init("turbocharger_generated/index_bg.wasm");
- append(await tc.wasm_get_greeting());
- append(await tc.backend_get_greeting());
+ append(await wasm.get_local_greeting());
+ append(await backend.get_remote_greeting());
 }
 
 main();
--- example-minimal/src/main ideal.rs
+++ example-minimal/src/main ideal.rs
@@ -1,19 +1,21 @@
-use turbocharger::prelude::*; // includes wasm_bindgen::prelude::*
+use turbocharger::{wasm, backend, server_only};
 
+/// Call from JavaScript as `await wasm.get_local_greeting()`
 #[wasm]
-pub async fn wasm_get_greeting() -> String {
+pub async fn get_local_greeting() -> String {
  "Hello from WASM.".to_string()
 }
 
+/// Call from JavaScript as `await backend.get_remote_greeting()`
 #[backend]
-pub async fn backend_get_greeting() -> String {
- println!("backend_get_greeting called");
+pub async fn get_remote_greeting() -> String {
+ eprintln!("get_remote_greeting called");
  "Hello from backend.".to_string()
 }
 
 #[server_only]
 #[tokio::main]
 async fn main() {
- println!("Serving on http://127.0.0.1:8080");
+ eprintln!("Serving on http://127.0.0.1:8080");
  warp::serve(turbocharger::warp_routes()).run(([127, 0, 0, 1], 8080)).await;
 }
