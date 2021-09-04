1d5081f613caebcd13c51c48c9b6a0eb12975589
trevyn <trevyn-git@protonmail.com>
2021-09-04 11:01:30 +0400

    rely on `turbocharger-template` for testing

--- example-turbosql/frontend/src/index.js
+++
@@ -1,8 +0,0 @@
-import turbocharger_init, * as backend from "./turbocharger_generated";
-
-(async () => {
- await turbocharger_init();
- let person = Object.assign(new backend.Person(), { name: "Bob" });
- let rowid = await backend.insert_person(person);
- console.log((await backend.get_person(rowid)).toJSON());
-})();
