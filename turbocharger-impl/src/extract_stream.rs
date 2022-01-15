67df20d0ee1f725db876c753f6b3fe3fed4f10bf
trevyn <trevyn-git@protonmail.com>
2022-01-15 12:06:15 +0300

    add working extract_stream

--- turbocharger-impl/src/extract_stream.rs
+++ turbocharger-impl/src/extract_stream.rs
@@ -1,36 +1,52 @@
-pub fn inner_ty(_orig_fn_ret_ty: syn::Type) -> Option<syn::GenericArgument> {
- // dbg!(orig_fn_ret_ty);
- None
- // let typepath = match orig_fn_ret_ty {
- //  syn::Type::Path(typepath) => Some(typepath),
- //  _ => None,
- // };
- // let path = match typepath {
- //  Some(syn::TypePath { path, .. }) => Some(path),
- //  _ => None,
- // };
- // let pair = match path {
- //  Some(syn::Path { mut segments, .. }) => segments.pop(),
- //  _ => None,
- // };
- // let (is_result, arguments) = match pair.map(|pair| pair.into_value()) {
- //  Some(syn::PathSegment { ident, arguments }) => (ident == "Result", Some(arguments)),
- //  _ => (false, None),
- // };
- // if !is_result {
- //  return None;
- // }
- // let anglebracketedgenericarguments = match arguments {
- //  Some(syn::PathArguments::AngleBracketed(anglebracketedgenericarguments)) => {
- //   Some(anglebracketedgenericarguments)
- //  }
- //  _ => None,
- // };
- // let args = match anglebracketedgenericarguments {
- //  Some(syn::AngleBracketedGenericArguments { args, .. }) => Some(args),
- //  _ => None,
- // };
- // args.map(|args| args.into_iter().next()).flatten()
+pub fn inner_ty(orig_fn_ret_ty: syn::Type) -> Option<syn::Ident> {
+ let impltrait = match orig_fn_ret_ty {
+  syn::Type::ImplTrait(impltrait) => Some(impltrait),
+  _ => return None,
+ };
+ let bounds = match impltrait {
+  Some(syn::TypeImplTrait { bounds, .. }) => Some(bounds),
+  _ => return None,
+ };
+ let path = match bounds.unwrap()[0].clone() {
+  syn::TypeParamBound::Trait(syn::TraitBound { path, .. }) => Some(path),
+  _ => return None,
+ };
+ let segments = match path {
+  Some(syn::Path { segments, .. }) => Some(segments),
+  _ => return None,
+ };
+ let pathsegment = segments.unwrap()[0].clone();
+ if pathsegment.ident != "Stream" {
+  return None;
+ }
+ let anglebracketedgenericarguments = match pathsegment.arguments {
+  syn::PathArguments::AngleBracketed(anglebracketedgenericarguments) => {
+   Some(anglebracketedgenericarguments)
+  }
+  _ => return None,
+ };
+ let args = match anglebracketedgenericarguments {
+  Some(syn::AngleBracketedGenericArguments { args, .. }) => Some(args),
+  _ => return None,
+ };
+ let arg = args.unwrap()[0].clone();
+ let binding = match arg {
+  syn::GenericArgument::Binding(binding) => Some(binding),
+  _ => return None,
+ };
+ let syn::Binding { ty, .. } = binding.unwrap();
+ let path = match ty {
+  syn::Type::Path(path) => Some(path),
+  _ => return None,
+ };
+ let typepath = match path {
+  Some(syn::TypePath { path, .. }) => Some(path),
+  _ => return None,
+ };
+ let syn::Path { segments, .. } = typepath.unwrap();
+ let segment = segments[0].clone();
+
+ Some(segment.ident)
 }
 
 #[cfg(test)]
@@ -41,7 +57,7 @@ mod tests {
  fn test_extract_stream() {
   assert_eq!(
    inner_ty(syn::parse_str::<syn::Type>("impl Stream<Item = u32>").unwrap()),
-   Some(syn::parse_str::<syn::GenericArgument>("u32").unwrap())
+   Some(syn::parse_str::<syn::Ident>("u32").unwrap())
   );
   assert_eq!(inner_ty(syn::parse_str::<syn::Type>("u32").unwrap()), None);
  }
