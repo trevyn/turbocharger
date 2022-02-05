16e9c68796433df460664823d7f5fd1405e79c07
trevyn <trevyn-git@protonmail.com>
2022-02-05 15:36:12 +0300

    allow setting the websocket url

--- turbocharger/src/lib.rs
+++ turbocharger/src/lib.rs
@@ -33,13 +33,15 @@ pub trait RPC: Send + Sync {
 struct Globals {
  #[cfg_attr(not(target_arch = "wasm32"), allow(dead_code))]
  channel_tx: Option<futures::channel::mpsc::UnboundedSender<Vec<u8>>>,
+ #[cfg_attr(not(target_arch = "wasm32"), allow(dead_code))]
+ socket_url: Option<String>,
  next_txid: i64,
  senders: std::collections::HashMap<i64, futures::channel::mpsc::UnboundedSender<Vec<u8>>>,
 }
 
 impl Default for Globals {
  fn default() -> Self {
-  Self { channel_tx: None, next_txid: 256, senders: Default::default() }
+  Self { socket_url: None, channel_tx: None, next_txid: 256, senders: Default::default() }
  }
 }
 
@@ -118,6 +120,7 @@ impl _Transaction {
  #[cfg(target_arch = "wasm32")]
  pub fn send_ws(&self, req: Vec<u8>) {
   wasm_bindgen_futures::spawn_local(async move {
+   ensure_ws_connected().await;
    let mut channel = G.lock().unwrap().channel_tx.clone().unwrap();
    channel.send(req).await.unwrap();
   });
@@ -195,50 +198,65 @@ pub async fn spawn_udp_server(port: u16) -> Result<(), Box<dyn std::error::Error
 }
 
 #[wasm_only]
+#[wasm_bindgen]
+pub fn set_socket_url(url: String) {
+ let mut g = G.lock().unwrap();
+ g.socket_url = Some(url);
+}
+
+#[wasm_only]
+async fn ensure_ws_connected() {
+ let mut g = G.lock().unwrap();
+
+ if g.socket_url.is_none() {
+  g.socket_url = Some({
+   let location = web_sys::window().unwrap().location();
+   let protocol = match location.protocol().unwrap().as_str() {
+    "https:" => "wss:",
+    _ => "ws:",
+   };
+   format!("{}//{}/turbocharger_socket", protocol, location.host().unwrap())
+  });
+ }
+ let socket_url = g.socket_url.clone().unwrap();
+
+ if g.channel_tx.is_none() {
+  let (channel_tx, mut channel_rx) = futures::channel::mpsc::unbounded();
+  g.channel_tx = Some(channel_tx);
+
+  std::mem::drop(g);
+
+  tc_console_log!("connecting to {}", socket_url);
+
+  let (_ws, wsio) = ws_stream_wasm::WsMeta::connect(socket_url, None).await.unwrap();
+
+  tc_console_log!("connected");
+
+  let (mut ws_tx, mut ws_rx) = wsio.split();
+
+  wasm_bindgen_futures::spawn_local(async move {
+   while let Some(msg) = ws_rx.next().await {
+    if let ws_stream_wasm::WsMessage::Binary(msg) = msg {
+     let txid = i64::from_le_bytes(msg[0..8].try_into().unwrap());
+     let mut sender = G.lock().unwrap().senders.get(&txid).unwrap().clone();
+     sender.send(msg).await.unwrap();
+    }
+   }
+   tc_console_log!("ws_rx ENDED");
+  });
+
+  wasm_bindgen_futures::spawn_local(async move {
+   while let Some(msg) = channel_rx.next().await {
+    ws_tx.send(ws_stream_wasm::WsMessage::Binary(msg)).await.unwrap();
+   }
+   tc_console_log!("rx ENDED");
+  });
+ }
+}
+
+#[wasm_only]
 #[wasm_bindgen(start)]
 pub async fn start() -> Result<(), JsValue> {
  console_error_panic_hook::set_once();
-
- let (channel_tx, mut channel_rx) = futures::channel::mpsc::unbounded();
- G.lock().unwrap().channel_tx = Some(channel_tx);
-
- #[cfg(turbocharger_test)]
- let socket_url = "ws://localhost:8080/turbocharger_socket";
- #[cfg(not(turbocharger_test))]
- let socket_url = {
-  let location = web_sys::window().unwrap().location();
-  let protocol = match location.protocol().unwrap().as_str() {
-   "https:" => "wss:",
-   _ => "ws:",
-  };
-  format!("{}//{}/turbocharger_socket", protocol, location.host().unwrap())
- };
-
- tc_console_log!("connecting to {}", socket_url);
-
- let (_ws, wsio) = ws_stream_wasm::WsMeta::connect(socket_url, None).await.unwrap();
-
- tc_console_log!("connected");
-
- let (mut ws_tx, mut ws_rx) = wsio.split();
-
- wasm_bindgen_futures::spawn_local(async move {
-  while let Some(msg) = ws_rx.next().await {
-   if let ws_stream_wasm::WsMessage::Binary(msg) = msg {
-    let txid = i64::from_le_bytes(msg[0..8].try_into().unwrap());
-    let mut sender = G.lock().unwrap().senders.get(&txid).unwrap().clone();
-    sender.send(msg).await.unwrap();
-   }
-  }
-  tc_console_log!("ws_rx ENDED");
- });
-
- wasm_bindgen_futures::spawn_local(async move {
-  while let Some(msg) = channel_rx.next().await {
-   ws_tx.send(ws_stream_wasm::WsMessage::Binary(msg)).await.unwrap();
-  }
-  tc_console_log!("rx ENDED");
- });
-
  Ok(())
 }
