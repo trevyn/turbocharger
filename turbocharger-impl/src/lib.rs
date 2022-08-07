ee5eadcf9f6cb8f7df0322178424fef24b4f0593
trevyn <230691+trevyn@users.noreply.github.com>
2022-08-07 15:08:16 -0400

    understand new `rust-analyzer-proc-macro-srv` binary

--- turbocharger-impl/src/lib.rs
+++ turbocharger-impl/src/lib.rs
@@ -146,7 +146,13 @@ fn backend_item(args: proc_macro::TokenStream, orig_item: syn::Item) -> proc_mac
 // }
 
 fn backend_struct(orig_struct: syn::ItemStruct) -> proc_macro2::TokenStream {
- if std::env::current_exe().unwrap().file_stem().unwrap() != "rust-analyzer" {
+ if !std::env::current_exe()
+  .unwrap()
+  .file_stem()
+  .unwrap()
+  .to_string_lossy()
+  .starts_with("rust-analyzer")
+ {
   let mut api_struct = orig_struct.clone();
   api_struct.vis = parse_quote!();
   api_struct.attrs.retain(|attr| attr.path.is_ident("doc"));
@@ -228,7 +234,13 @@ fn backend_struct(orig_struct: syn::ItemStruct) -> proc_macro2::TokenStream {
 fn backend_fn(args: proc_macro::TokenStream, orig_fn: syn::ItemFn) -> proc_macro2::TokenStream {
  let is_js = args.to_string() == "js";
 
- if std::env::current_exe().unwrap().file_stem().unwrap() != "rust-analyzer" {
+ if !std::env::current_exe()
+  .unwrap()
+  .file_stem()
+  .unwrap()
+  .to_string_lossy()
+  .starts_with("rust-analyzer")
+ {
   let mut api_fn = orig_fn.clone();
   api_fn.vis = parse_quote!();
   api_fn.attrs.retain(|attr| attr.path.is_ident("doc"));
